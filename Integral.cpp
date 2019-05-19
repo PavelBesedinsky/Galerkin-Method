9d38729d85e1f87309532b0b1c10b219599c1575
Pavel <pavelbesedinsky@gmail.com>
2019-05-19 23:00:43 +0300

    Добавил методы для двух функций

--- Integral.cpp
+++ Integral.cpp
@@ -9,6 +9,9 @@ void Integral::setStep()
 
 void Integral::setStep(int N, double a, double b)
 {
+	_N = N;
+	_a = a;
+	_b = b;
 	_step = (_b - _a) / _N;
 }
 
@@ -32,6 +35,31 @@ double Integral::Rectangle(double(&Function)(double), int N, double a, double b)
 	for (double i = 0; i < _N; i++)
 	{
 		Sum += Function(X);
+		X += _step;
+	}
+	return Sum * _step;
+}
+
+double Integral::Rectangle(double(&Function1)(double), double(&Function2)(double))
+{
+	double Sum = 0;
+	double X = _a + 0.5 * _step;
+	for (double i = 0; i < _N; i++)
+	{
+		Sum += Function1(X) * Function2(X);
+		X += _step;
+	}
+	return Sum * _step;
+}
+
+double Integral::Rectangle(double(&Function1)(double), double(&Function2)(double), int N, double a, double b)
+{
+	setStep(N, a, b);
+	double Sum = 0;
+	double X = _a + 0.5 * _step;
+	for (int i = 0; i < _N; i++)
+	{
+		Sum += Function1(X) * Function2(X);
 		X += _step;
 	}
 	return Sum * _step;
--- Integral.h
+++ Integral.h
@@ -1,4 +1,6 @@
 #pragma once
+#include <stdio.h>
+#include <math.h>
 class Integral
 {
 private:
@@ -12,6 +14,9 @@ public:
 	double Rectangle(double(&Function)(double));
 	double Rectangle(double(&Function)(double), int N, double a, double b);
 	
+	double Rectangle(double(&Function1)(double), double(&Function2)(double));
+	double Rectangle(double(&Function1)(double), double(&Function2)(double), int N, double a, double b);
+
 	Integral();
 	Integral(int N, double a, double b);
 	~Integral();
