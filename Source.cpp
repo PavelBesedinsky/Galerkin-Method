7b6b8de3228bfb237f847f865b9d4fa36f486350
Pavel <pavelbesedinsky@gmail.com>
2019-05-19 23:17:57 +0300

    Новая реализация

--- Source.cpp
+++ Source.cpp
@@ -2,44 +2,36 @@
 #include <math.h>
 #include "Derivative.h"
 #include "Integral.h"
-#include "Funcs.h"
 #include <windows.h>
-
-double L0(double x)
-{
-	return U0(x) * LU0(x);
-}
-double L1(double x)
-{
-	return U1(x) * LU1(x);
-}
-double L2(double x)
-{
-	return U2(x) * LU2(x);
-}
-double L3(double x)
-{
-	return U2(x) * LU2(x);
-}
-double L4(double x)
-{
-	return U2(x) * LU2(x);
-}
-double Lf(double x)
-{
-	return fLU(x);
-}
+#include "Funcs2.h"
 
 int main(int argc, char **argv)
 {
 	typedef double(*arrayFunctions)(double x);
-	arrayFunctions arrF[5][5];
-	arrF[0][0] = L0;
-	arrF[0][1] = L1;
+	arrayFunctions A[4][4];
+	A[0][0] = L11;
+	A[0][1] = L12;
+	A[0][2] = L13;
+	A[0][3] = L14;
+
+	A[1][0] = L21;
+	A[1][1] = L22;
+	A[1][2] = L23;
+	A[1][3] = L24;
+
+	A[2][0] = L31;
+	A[2][1] = L32;
+	A[2][2] = L33;
+	A[2][3] = L34;
+
+	A[3][0] = L41;
+	A[3][1] = L42;
+	A[3][2] = L43;
+	A[3][3] = L44;
 
 	Derivative* exp = new Derivative();
 	Integral* integ = new Integral();
-	double result = integ->Rectangle(*arrF[0][1], 1000, 1.0, 2.0);
+	double result = integ->Rectangle(*A[0][3], 1000, 1.0, 2.0);
 	printf("Result = %f\n", result);
 	system("pause");
 	delete exp;
